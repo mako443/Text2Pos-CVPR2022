094b4f41cf6cd845adbd6c412a2990a25d825d78
Manuel Kolmet <manuel.kolmet@gmail.com>
2021-11-10 14:23:45 +0100

    Updated visloc prepare, loading, eval. Real images at near-perfect now.

--- dataloading/kitti360/images.py
+++ dataloading/kitti360/images.py
@@ -13,48 +13,59 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 
 class Kitti360ImageDataset(Dataset):
-    def __init__(self, base_path, scene_name, db_or_query, query_fraction=4, transform=None):
-        assert db_or_query in ('db', 'query')
+    def __init__(self, base_path, scene_name, split, transform=None):
+        assert split in ('db', 'query')
 
         self.base_path = base_path
         self.scene_name = scene_name
+        self.split = split
         self.transform = transform
 
-        with open(osp.join(base_path, scene_name, 'poses.pkl'), 'rb') as f:
+        with open(osp.join(base_path, scene_name, f'poses_{split}.pkl'), 'rb') as f:
             self.poses = pickle.load(f)
 
-        with open(osp.join(base_path, scene_name, 'orientations.pkl'), 'rb') as f:
-            self.orientations = pickle.load(f)
+        # NOTE: File names have to be sorted!
+        self.image_paths = np.array(sorted([
+            osp.join(base_path, scene_name, 'real', split, f) for f in os.listdir(osp.join(base_path, scene_name, 'real', split)) if f.endswith('.png')
+        ]))
 
-        self.image_paths = np.array([
-            osp.join(base_path, scene_name, 'image_00', f) for f in os.listdir(osp.join(base_path, scene_name, 'image_00')) if f.endswith('.png')
-        ])
+        if osp.isdir(osp.join(base_path, scene_name, 'rendered', split)):
+            self.image_paths_rendered = np.array(sorted([
+                osp.join(base_path, scene_name, 'rendered', split, f) for f in os.listdir(osp.join(base_path, scene_name, 'rendered', split)) if f.endswith('.png')
+            ]))
+        else:
+            self.image_paths_rendered = None
         
-        mask = np.array([True for _ in range(len(self.poses))])
-        mask[: : query_fraction] = False
-        if db_or_query == 'query':
-            mask = np.invert(mask)
 
-        self.poses, self.orientations, self.image_paths = self.poses[mask], self.orientations[mask], self.image_paths[mask]
-
-        assert len(self.poses) == len(self.orientations) == len(self.image_paths), 'Number of poses, orientations and images does not match.'
+        assert len(self.poses) == len(self.image_paths), 'Number of poses and images do not match.'
+        if self.image_paths_rendered is not None:
+            assert len(self.image_paths) == len(self.image_paths_rendered)
 
     def __len__(self):
         return len(self.poses)
 
     def __getitem__(self, idx):
         img = Image.open(self.image_paths[idx])
+        if self.image_paths_rendered is not None:
+            img_rendered = Image.open(self.image_paths_rendered[idx])
+        else:
+            img_rendered = None
 
         if self.transform:
             img = self.transform(img)
+            if img_rendered is not None:
+                img_rendered = self.transform(img_rendered)
 
-        return {
+        data = {
             'poses': self.poses[idx],
-            'orientations': self.orientations[idx],
-            'images': img
+            'images': img,
         }
+        if img_rendered is not None:
+            data['images_rendered'] = img_rendered
+
+        return data
 
 if __name__ == '__main__':
-    dataset = Kitti360ImageDataset('./data/k360_visloc_dist25', '2013_05_28_drive_0010_sync', 'db')
+    dataset = Kitti360ImageDataset('./data/k360-visloc_db-25_q5', '2013_05_28_drive_0010_sync', 'db')
     data = dataset[0]
     print(data)
--- datapreparation/kitti360/prepare_images.py
+++ datapreparation/kitti360/prepare_images.py
@@ -12,7 +12,13 @@ from shutil import copyfile
 
 from scipy.spatial.distance import cdist
 
-# import open3d
+'''
+TODO
+- Split into DB / Query right away for cleanup, add to dataset-name
+    - dataset/real-rendered/db-query
+    - db-dist, query-dist, step only every 4th or something
+- Interpolate orientations based on *all* poses    
+'''
 
 def sample_poses(path_poses, pose_distance):
     poses = np.loadtxt(path_poses)
@@ -38,8 +44,41 @@ def sample_poses(path_poses, pose_distance):
 
     return np.array(sampled_poses), np.array(sampled_orientations), np.array(sampled_image_names)
 
-def gather_images(path_images, ):
-    pass
+def create_poses_and_images(path_poses, path_images, path_out, db_dist=20, query_dist=5, step=4):
+    poses = np.loadtxt(path_poses)
+    image_names = np.int0(poses[:, 0])
+
+    path_db = osp.join(path_out, 'real', 'db')
+    path_query = osp.join(path_out, 'real', 'query')
+    os.makedirs(path_db)
+    os.makedirs(path_query)
+
+    poses = poses[:, 1:].reshape((-1, 3,4)) # Convert to 3x4 matrices
+    poses = poses[:, :, -1] # Take last column
+
+    sampled_db_poses = [poses[0], ]
+    image_name = image_names[0]
+    copyfile(osp.join(path_images, f'{image_name:010.0f}.png'), osp.join(path_db, f'{len(sampled_db_poses) - 1:04.0f}.png'))
+
+    sampled_query_poses = []
+    for idx in range(0, len(poses), step):
+        pose, image_name = poses[idx], image_names[idx]
+
+        dists = np.linalg.norm(pose - sampled_db_poses, axis=1)
+        if np.min(dists) >= db_dist:
+            sampled_db_poses.append(pose)
+            copyfile(osp.join(path_images, f'{image_name:010.0f}.png'), osp.join(path_db, f'{len(sampled_db_poses) - 1:04.0f}.png'))
+        elif np.min(dists) >= query_dist:
+            sampled_query_poses.append(pose)
+            copyfile(osp.join(path_images, f'{image_name:010.0f}.png'), osp.join(path_query, f'{len(sampled_query_poses) - 1:04.0f}.png'))
+
+    with open(osp.join(path_out, 'poses_db.pkl'), 'wb') as f:
+        pickle.dump(np.array(sampled_db_poses), f)
+    with open(osp.join(path_out, 'poses_query.pkl'), 'wb') as f:
+        pickle.dump(np.array(sampled_query_poses), f)   
+
+    print(f'Saved {len(sampled_db_poses)} / {len(sampled_query_poses)} poses.')
+        
 
 if __name__ == '__main__':
     np.random.seed(4096) # Set seed to re-produce results
@@ -47,31 +86,36 @@ if __name__ == '__main__':
     folder_name = '2013_05_28_drive_0010_sync' # Only on validation set
 
     path_poses = osp.join('./data/kitti360/data_poses', folder_name, 'poses.txt')
-    path_images = osp.join('./data/kitti360-images/kitti360-data-2d', folder_name)
-    pose_dist = 25 # In meters
+    path_images = osp.join('./data/kitti360-images/kitti360-data-2d', folder_name, 'image_00', 'data_rect')
+    db_dist = 25 # Distance between db poses
+    query_dist = 5 # Distance a query pose has to have to the next db pose
 
-    path_out = f'./data/k360_visloc_dist{pose_dist}/{folder_name}'
+    path_out = f'./data/k360-visloc_db-{db_dist}_q{query_dist}/{folder_name}'
 
     if osp.isdir(path_out):
         quit('Output directory already exists!')
-    
-    poses, orientations, image_names = sample_poses(path_poses, pose_dist)
-    print(f'Num poses: {len(poses)}')
 
-    # Save / copy data
-    os.mkdir(path_out)
+    os.makedirs(path_out)
 
-    with open(osp.join(path_out, 'poses.pkl'), 'wb') as f:
-        pickle.dump(poses, f)
-    with open(osp.join(path_out, 'orientations.pkl'), 'wb') as f:
-        pickle.dump(orientations, f)
+    create_poses_and_images(path_poses, path_images, path_out, db_dist, query_dist)
 
-    os.mkdir(osp.join(path_out, 'image_00'))
-    for target_name, image_name in enumerate(image_names):
-        path_src = osp.join(path_images, 'image_00', 'data_rect', f'{image_name:010.0f}.png')
-        path_dst = osp.join(path_out, 'image_00', f'{target_name:04.0f}.png')
-        copyfile(path_src, path_dst)
-    print('DONE.')
+    # poses, orientations, image_names = sample_poses(path_poses, pose_dist)
+    # print(f'Num poses: {len(poses)}')
+
+    # # Save / copy data
+    # os.mkdir(path_out)
+
+    # with open(osp.join(path_out, 'poses.pkl'), 'wb') as f:
+    #     pickle.dump(poses, f)
+    # with open(osp.join(path_out, 'orientations.pkl'), 'wb') as f:
+    #     pickle.dump(orientations, f)
+
+    # os.mkdir(osp.join(path_out, 'image_00'))
+    # for target_name, image_name in enumerate(image_names):
+    #     path_src = osp.join(path_images, 'image_00', 'data_rect', f'{image_name:010.0f}.png')
+    #     path_dst = osp.join(path_out, 'image_00', f'{target_name:04.0f}.png')
+    #     copyfile(path_src, path_dst)
+    # print('DONE.')
             
 
     
